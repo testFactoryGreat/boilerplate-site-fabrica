fd75c450553c3e6af362a7dcd8022ccfd5615750
lcarvalhodev <lcarvalho.dev@gmail.com>
2020-07-17 02:45:45 -0300

    almost finish carousel

---
+++ articles/article-3 copy.md
@@ -0,0 +1,6 @@
+---
+title: article 3
+resume: article 3
+link: https://www.researchgate.net/publication/337779457_Extraction_of_test_cases_procedures_from_textual_use_cases_to_reduce_test_effort_Test_Factory_Experience_Report
+date: 2020-07-17 01:29:05
+---
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -2,17 +2,27 @@ import React from "react"
 import Title from "./Title"
 import Blog from "./Blog"
 import { Link } from "gatsby"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Slider from "react-slick";
+import { Carousel } from "react-responsive-carousel";
 
 export const Blogs = ({ articles, title }) => {
 
   return <section id="articles" className="article-page">
     <Title title={title} subtitle="Pesquisas que resultaram em artigos publicados pela nossa equipe" />
-    <div className="section-center blogs-center">
-      {articles.map((article) => {
-        return <Blog key={article.id} {...article.frontmatter} />
-      })}
+
+    <div className="section-center ">
+      <Carousel showThumbs={false} showStatus={false}>
+        {articles.map((article) => {
+          return (
+            <div>
+              <Blog key={article.id} {...article.frontmatter} />
+            </div>
+          )
+        })}
+      </Carousel>
     </div>
-  </section>
+
+  </section >
 }
 export default Blogs
